f659315e50dfaeb1bd671a8e7ee6d902360ab69e
AmruthaBC <ammu14598@gmail.com>
2021-03-23 17:52:12 +0530

    initial commit

---
+++ include/header.php
@@ -0,0 +1,146 @@
+<header id="menu" class="menu"> 
+
+	<div class="container-fluid container-fluid--big"> 
+		<a class="menu__logo" href="index.php"> 
+			<img class="menu__logo__white" src="assets/imgs/liberty_logo.png" alt="Liberty" /> 
+			<img  class="menu__logo__black"  src="assets/imgs/liberty_logo.png" alt="Liberty" /> 
+		</a> 
+	</div> 
+
+	<div class="menu__top"> 
+		<nav class="menu__top__primary"> 
+			<ul class="menu__top__primary__pages"> 
+				<li class="menu__top__primary__page menu__top__primary__page--childrens"> 
+					<a class="" href="#">LIBERTY</a> 
+					<ul class="menu__top__primary__pages__children"> 
+						<li> <a class="" href="historia-grupo-nabeiro.html">History</a> </li> 
+						<li> <a class="" href="visao-grupo-nabeiro.html">Vision & Mission</a> </li> 
+						<li> <a class="" href="fundador.html">Founder</a> </li> 
+						<li> <a class="" href="grupo-no-mundo.html">Worldwide Export</a> </li> 
+						<li> <a class="" href="organograma-grupo-nabeiro.html">Organizational Chart</a> </li>
+						<li> <a class="" href="premios-e-distincoes.html">Awards & Certifications</a> </li> 
+					</ul> 
+				</li> 
+				<li class="menu__top__primary__page menu__top__primary__page--childrens"> <a class="" href="#">BUSINESS</a> 
+					<ul class="menu__top__primary__pages__children"> 
+						<li> <a class="" href="empresas.html">Empresas</a> </li> 
+						<li> <a class="" href="marcas-proprias.html">Marcas Próprias</a> </li> 
+						<li> <a class="" href="marcas-representadas.html">Marcas Representadas</a> </li> 
+					</ul> 
+			    </li> 
+				<li class="menu__top__primary__page "> 
+					<a class="" href="sustentabilidade.html">PEOPLE</a> 
+				</li> 
+				<li class="menu__top__primary__page menu__top__primary__page--childrens"> 
+					<a class="" href="#">CSR</a> 
+					<ul class="menu__top__primary__pages__children"> 
+						<li> <a class="" href="diverge.html">Diverge – Centro de Inovação do Grupo Nabeiro</a> </li> 
+						<li> <a class="" href="centro-de-ciencia-do-cafe.html">Centro de Ciência do Café</a> </li> 
+					</ul> 
+				</li> 
+				<li class="menu__top__primary__page menu__top__primary__page--childrens"> <a class="" href="#">NEWSROOM</a> 
+					<ul class="menu__top__primary__pages__children"> 
+					<li> <a class="" href="carreiras.html">Carreiras</a> </li> 
+					<li> <a class="" href="candidaturas.html">Candidaturas</a> </li> 
+					<li> <a class="" href="formacao-e-desenvolvimento.html">Formação e Desenvolvimento</a> </li> 
+					</ul> 
+			    </li> 
+				<li class="menu__top__primary__page menu__top__primary__page--childrens"> <a class="" href="#">CAREERS</a> 
+					<ul class="menu__top__primary__pages__children"> 
+						<li> <a class="" href="comunicados-de-imprensa.html">Comunicados de Imprensa</a> </li> 
+						<li> <a class="" href="noticias.html">Notícias</a> </li> 
+						<li> <a class="" href="media-kit.html">Media Kit</a> </li> 
+					</ul> 
+				</li> 
+			</ul> 
+		</nav> 
+
+		<div class="container-fluid container-fluid--big position-relative"> 
+			<button data-lottie="" class="menu__top__search-btn" type="submit" aria-expanded="false" > <span class="sr-only"></span> 
+		    </button>
+		</div> 
+
+		<form class="menu__top__search" id="top_search_form" role="search" action="" method="get"> 
+			<div class="container-fluid container-fluid--big position-relative"> 
+				<input class="menu__top__search__input" name="s" type="text" placeholder="Diga-nos o que procura…." autocomplete="off" value="" aria-label="Pesquisar" spellcheck="false" /> 
+				<button class="menu__top__search__submit" type="submit" aria-expanded="false" aria-label="Pesquisar"> </button> 
+			</div> 
+		</form> 
+	</div> 
+
+	<div class="container-fluid container-fluid--big">
+		<div class="menu__hamburger-wrap"> 
+			<button data-hamburger-btn="" type="button" class="menu__hamburger" aria-label="Abrir menu"> 
+				<span class="hamburger">
+				<span class="hamburger__center"></span> 
+				</span> 
+			</button> 
+		</div> 
+	</div> 
+</header> 
+<div class="modal-menu"> 
+	<div class="modal-menu__bg">
+		<div class="modal-menu__bg__arrows"></div> 
+	</div> 
+
+	<div class="container-fluid container-fluid--big position-relative">
+		<form class="modal-menu__search " role="search" action="" method="get"> 
+		<input id="modal-menu-input" class="modal-menu__search__input d-none d-md-block" name="s" type="text" placeholder="Search" autocomplete="off" value="" aria-label="Search" spellcheck="false" /> <button class="modal-menu__search__submit d-none d-md-block" data-focus-if-empty="#modal-menu-input" type="submit" aria-expanded="false" aria-label="Search"> <svg class=""><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#sprite-search"></use></svg> </button> <button class="modal-menu__search__submit d-md-none" type="submit" aria-expanded="false" aria-label="Search"> <svg class=""><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#sprite-search"></use></svg> </button> 
+		</form> 
+	</div> 
+
+	<div class="container-fluid container-fluid--big modal-menu__content"> 
+		<div class="modal-menu__nav-wrap"> 
+			<nav class="modal-menu__nav "> 
+				<div class="modal-menu__nav__pages row mt-n40 mt-md-n70">
+					<div class="modal-menu__nav__page mt-30 mt-md-50 pt-md-10 col-md-6 col-lg-4 modal-menu__nav__page--childrens"> <a class=" type--20-32" href="#" data-self-toggle="opened">LIBERTY</a> 
+						<ul class="modal-menu__nav__pages__children ml-20 d-none d-md-block"> 
+							<li class="mt-20"> <a class=" type--13 type--bold" href="historia-grupo-nabeiro.html">History</a> </li> 
+							<li class="mt-20"> <a class=" type--13 type--bold" href="visao-grupo-nabeiro.html">Vision & Mission</a> </li> 
+							<li class="mt-20"> <a class=" type--13 type--bold" href="fundador.html">Founder</a> </li> 
+							<li class="mt-20"> <a class=" type--13 type--bold" href="grupo-no-mundo.html">Worldwide Export</a> </li> 
+							<li class="mt-20"> <a class=" type--13 type--bold" href="organograma-grupo-nabeiro.html">Organizational Chart</a> </li> 
+							<li class="mt-20"> <a class=" type--13 type--bold" href="premios-e-distincoes.html">Awards & Certifications</a> </li> 
+						</ul> 
+					</div> 
+
+					<div class="modal-menu__nav__page mt-30 mt-md-50 pt-md-10 col-md-6 col-lg-4 modal-menu__nav__page--childrens"> <a class=" type--20-32" href="#" data-self-toggle="opened">BUSINESS</a> 
+						<ul class="modal-menu__nav__pages__children ml-20 d-none d-md-block"> 
+						<li class="mt-20"> <a class=" type--13 type--bold" href="empresas.html">Empresas</a> </li> 
+						<li class="mt-20"> <a class=" type--13 type--bold" href="marcas-proprias.html">Marcas Próprias</a> </li> 
+						<li class="mt-20"> <a class=" type--13 type--bold" href="marcas-representadas.html">Marcas Representadas</a> </li> 
+						</ul> 
+					</div> 
+
+					<div class="modal-menu__nav__page mt-30 mt-md-50 pt-md-10 col-md-6 col-lg-4 "> 
+						<a class=" type--20-32" href="sustentabilidade.html" >PEOPLE</a> 
+					</div> 
+
+					<div class="modal-menu__nav__page mt-30 mt-md-50 pt-md-10 col-md-6 col-lg-4 modal-menu__nav__page--childrens"> <a class=" type--20-32" href="#" data-self-toggle="opened">CSR</a> 
+						<ul class="modal-menu__nav__pages__children ml-20 d-none d-md-block"> 
+							<li class="mt-20"> <a class=" type--13 type--bold" href="diverge.html">Diverge – Centro de Inovação do Grupo Nabeiro</a> </li> 
+							<li class="mt-20"> <a class=" type--13 type--bold" href="centro-de-ciencia-do-cafe.html">Centro de Ciência do Café</a> </li> 
+						</ul> 
+					</div> 
+
+					<div class="modal-menu__nav__page mt-30 mt-md-50 pt-md-10 col-md-6 col-lg-4 modal-menu__nav__page--childrens"> <a class=" type--20-32" href="#" data-self-toggle="opened">NEWSROOM</a> 
+						<ul class="modal-menu__nav__pages__children ml-20 d-none d-md-block"> 
+							<li class="mt-20"> <a class=" type--13 type--bold" href="carreiras.html">Carreiras</a> </li> 
+							<li class="mt-20"> <a class=" type--13 type--bold" href="candidaturas.html">Candidaturas</a> </li> 
+							<li class="mt-20"> <a class=" type--13 type--bold" href="formacao-e-desenvolvimento.html">Formação e Desenvolvimento</a> </li> 
+						</ul> 
+					</div> 
+
+					<div class="modal-menu__nav__page mt-30 mt-md-50 pt-md-10 col-md-6 col-lg-4 modal-menu__nav__page--childrens"> <a class=" type--20-32" href="#" data-self-toggle="opened">CAREERS</a> 
+						<ul class="modal-menu__nav__pages__children ml-20 d-none d-md-block"> 
+							<li class="mt-20"> <a class=" type--13 type--bold" href="comunicados-de-imprensa.html">Comunicados de Imprensa</a> </li> 
+							<li class="mt-20"> <a class=" type--13 type--bold" href="noticias.html">Notícias</a> </li> 
+							<li class="mt-20"> <a class=" type--13 type--bold" href="media-kit.html">Media Kit</a> </li> 
+						</ul> 
+					</div> 
+				</div> 
+			</nav> 
+		</div> 
+	</div> 
+</div>
+
