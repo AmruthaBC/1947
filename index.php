58a131257ee179d0801ca87f8f77aa92db8fbdb7
AmruthaBC <ammu14598@gmail.com>
2021-03-23 17:34:23 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,365 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=0">
+    <meta name="description" content="Lewis - Creative Portfolio HTML Template">
+    <meta name="author" content="Paul, Logan Cee">
+
+    <title>Jain</title>
+
+    <!-- CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/animsition.min.css" rel="stylesheet">
+    <link href="css/owl.carousel.min.css" rel="stylesheet">
+    <link href="css/owl.theme.default.min.css" rel="stylesheet">
+    <link href="css/socicon.css" rel="stylesheet">
+    <link href="css/ionicons.min.css" rel="stylesheet">
+    <link href="css/animate.min.css" rel="stylesheet">
+    <link href="css/jquery.fancybox.min.css" rel="stylesheet">
+    <link href="css/jquery.pagepiling.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+
+    <!-- Main CSS -->
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/main.css" rel="stylesheet">
+
+    <!-- Favicons -->
+    <link rel="apple-touch-icon" sizes="180x180" href="favicon/logo.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon/logo.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon/logo.png">
+    <link rel="manifest" href="favicon/site.webmanifest">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+
+  </head>
+
+  <body class="theme-orange dark-horizontal">
+    <div class="animsition">
+      <div class="wrapper">
+        
+        <header id="menu" class="menu"> 
+
+          <div class="container-fluid container-fluid--big"> 
+            <a class="menu__logo" href="index.php"> 
+              <img class="menu__logo__white" src="assets/imgs/liberty_logo.png" alt="Liberty" /> 
+              <img  class="menu__logo__black"  src="assets/imgs/liberty_logo.png" alt="Liberty" /> 
+            </a> 
+          </div> 
+
+          <div class="menu__top"> 
+            <nav class="menu__top__primary"> 
+              <ul class="menu__top__primary__pages"> 
+                <li class="menu__top__primary__page menu__top__primary__page--childrens"> 
+                  <a class="" href="#">LIBERTY</a> 
+                  <ul class="menu__top__primary__pages__children"> 
+                    <li> <a class="" href="historia-grupo-nabeiro.html">History</a> </li> 
+                    <li> <a class="" href="visao-grupo-nabeiro.html">Vision & Mission</a> </li> 
+                    <li> <a class="" href="fundador.html">Founder</a> </li> 
+                    <li> <a class="" href="grupo-no-mundo.html">Worldwide Export</a> </li> 
+                    <li> <a class="" href="organograma-grupo-nabeiro.html">Organizational Chart</a> </li>
+                    <li> <a class="" href="premios-e-distincoes.html">Awards & Certifications</a> </li> 
+                  </ul> 
+                </li> 
+                <li class="menu__top__primary__page menu__top__primary__page--childrens"> <a class="" href="#">BUSINESS</a> 
+                  <ul class="menu__top__primary__pages__children"> 
+                    <li> <a class="" href="empresas.html">Empresas</a> </li> 
+                    <li> <a class="" href="marcas-proprias.html">Marcas Próprias</a> </li> 
+                    <li> <a class="" href="marcas-representadas.html">Marcas Representadas</a> </li> 
+                  </ul> 
+                  </li> 
+                <li class="menu__top__primary__page "> 
+                  <a class="" href="sustentabilidade.html">PEOPLE</a> 
+                </li> 
+                <li class="menu__top__primary__page menu__top__primary__page--childrens"> 
+                  <a class="" href="#">CSR</a> 
+                  <ul class="menu__top__primary__pages__children"> 
+                    <li> <a class="" href="diverge.html">Diverge – Centro de Inovação do Grupo Nabeiro</a> </li> 
+                    <li> <a class="" href="centro-de-ciencia-do-cafe.html">Centro de Ciência do Café</a> </li> 
+                  </ul> 
+                </li> 
+                <li class="menu__top__primary__page menu__top__primary__page--childrens"> <a class="" href="#">NEWSROOM</a> 
+                  <ul class="menu__top__primary__pages__children"> 
+                  <li> <a class="" href="carreiras.html">Carreiras</a> </li> 
+                  <li> <a class="" href="candidaturas.html">Candidaturas</a> </li> 
+                  <li> <a class="" href="formacao-e-desenvolvimento.html">Formação e Desenvolvimento</a> </li> 
+                  </ul> 
+                  </li> 
+                <li class="menu__top__primary__page menu__top__primary__page--childrens"> <a class="" href="#">CAREERS</a> 
+                  <ul class="menu__top__primary__pages__children"> 
+                    <li> <a class="" href="comunicados-de-imprensa.html">Comunicados de Imprensa</a> </li> 
+                    <li> <a class="" href="noticias.html">Notícias</a> </li> 
+                    <li> <a class="" href="media-kit.html">Media Kit</a> </li> 
+                  </ul> 
+                </li> 
+              </ul> 
+            </nav> 
+
+            <div class="container-fluid container-fluid--big position-relative"> 
+              <button data-lottie="" class="menu__top__search-btn" type="submit" aria-expanded="false" > <span class="sr-only"></span> 
+                </button>
+            </div> 
+
+            <form class="menu__top__search" id="top_search_form" role="search" action="" method="get"> 
+              <div class="container-fluid container-fluid--big position-relative"> 
+                <input class="menu__top__search__input" name="s" type="text" placeholder="Diga-nos o que procura…." autocomplete="off" value="" aria-label="Pesquisar" spellcheck="false" /> 
+                <button class="menu__top__search__submit" type="submit" aria-expanded="false" aria-label="Pesquisar"> </button> 
+              </div> 
+            </form> 
+          </div> 
+
+          <div class="container-fluid container-fluid--big">
+            <div class="menu__hamburger-wrap"> 
+              <button data-hamburger-btn="" type="button" class="menu__hamburger" aria-label="Abrir menu"> 
+                <span class="hamburger">
+                <span class="hamburger__center"></span> 
+                </span> 
+              </button> 
+            </div> 
+          </div> 
+      </header> 
+      <div class="modal-menu"> 
+        <div class="modal-menu__bg">
+          <div class="modal-menu__bg__arrows"></div> 
+        </div> 
+
+
+        <div class="container-fluid container-fluid--big modal-menu__content"> 
+          <div class="modal-menu__nav-wrap"> 
+            <nav class="modal-menu__nav "> 
+              <div class="modal-menu__nav__pages row mt-n40 mt-md-n70">
+                <div class="modal-menu__nav__page mt-30 mt-md-50 pt-md-10 col-md-6 col-lg-4 modal-menu__nav__page--childrens"> <a class=" type--20-32" href="#" data-self-toggle="opened">LIBERTY</a> 
+                  <ul class="modal-menu__nav__pages__children ml-20 d-none d-md-block"> 
+                    <li class="mt-20"> <a class=" type--13 type--bold" href="historia-grupo-nabeiro.html">History</a> </li> 
+                    <li class="mt-20"> <a class=" type--13 type--bold" href="visao-grupo-nabeiro.html">Vision & Mission</a> </li> 
+                    <li class="mt-20"> <a class=" type--13 type--bold" href="fundador.html">Founder</a> </li> 
+                    <li class="mt-20"> <a class=" type--13 type--bold" href="grupo-no-mundo.html">Worldwide Export</a> </li> 
+                    <li class="mt-20"> <a class=" type--13 type--bold" href="organograma-grupo-nabeiro.html">Organizational Chart</a> </li> 
+                    <li class="mt-20"> <a class=" type--13 type--bold" href="premios-e-distincoes.html">Awards & Certifications</a> </li> 
+                  </ul> 
+                </div> 
+
+                <div class="modal-menu__nav__page mt-30 mt-md-50 pt-md-10 col-md-6 col-lg-4 modal-menu__nav__page--childrens"> <a class=" type--20-32" href="#" data-self-toggle="opened">BUSINESS</a> 
+                  <ul class="modal-menu__nav__pages__children ml-20 d-none d-md-block"> 
+                  <li class="mt-20"> <a class=" type--13 type--bold" href="empresas.html">Empresas</a> </li> 
+                  <li class="mt-20"> <a class=" type--13 type--bold" href="marcas-proprias.html">Marcas Próprias</a> </li> 
+                  <li class="mt-20"> <a class=" type--13 type--bold" href="marcas-representadas.html">Marcas Representadas</a> </li> 
+                  </ul> 
+                </div> 
+
+                <div class="modal-menu__nav__page mt-30 mt-md-50 pt-md-10 col-md-6 col-lg-4 "> 
+                  <a class=" type--20-32" href="sustentabilidade.html" >PEOPLE</a> 
+                </div> 
+
+                <div class="modal-menu__nav__page mt-30 mt-md-50 pt-md-10 col-md-6 col-lg-4 modal-menu__nav__page--childrens"> <a class=" type--20-32" href="#" data-self-toggle="opened">CSR</a> 
+                  <ul class="modal-menu__nav__pages__children ml-20 d-none d-md-block"> 
+                    <li class="mt-20"> <a class=" type--13 type--bold" href="diverge.html">Diverge – Centro de Inovação do Grupo Nabeiro</a> </li> 
+                    <li class="mt-20"> <a class=" type--13 type--bold" href="centro-de-ciencia-do-cafe.html">Centro de Ciência do Café</a> </li> 
+                  </ul> 
+                </div> 
+
+                <div class="modal-menu__nav__page mt-30 mt-md-50 pt-md-10 col-md-6 col-lg-4 modal-menu__nav__page--childrens"> <a class=" type--20-32" href="#" data-self-toggle="opened">NEWSROOM</a> 
+                  <ul class="modal-menu__nav__pages__children ml-20 d-none d-md-block"> 
+                    <li class="mt-20"> <a class=" type--13 type--bold" href="carreiras.html">Carreiras</a> </li> 
+                    <li class="mt-20"> <a class=" type--13 type--bold" href="candidaturas.html">Candidaturas</a> </li> 
+                    <li class="mt-20"> <a class=" type--13 type--bold" href="formacao-e-desenvolvimento.html">Formação e Desenvolvimento</a> </li> 
+                  </ul> 
+                </div> 
+
+                <div class="modal-menu__nav__page mt-30 mt-md-50 pt-md-10 col-md-6 col-lg-4 modal-menu__nav__page--childrens"> <a class=" type--20-32" href="#" data-self-toggle="opened">CAREERS</a> 
+                  <ul class="modal-menu__nav__pages__children ml-20 d-none d-md-block"> 
+                    <li class="mt-20"> <a class=" type--13 type--bold" href="comunicados-de-imprensa.html">Comunicados de Imprensa</a> </li> 
+                    <li class="mt-20"> <a class=" type--13 type--bold" href="noticias.html">Notícias</a> </li> 
+                    <li class="mt-20"> <a class=" type--13 type--bold" href="media-kit.html">Media Kit</a> </li> 
+                  </ul> 
+                </div> 
+              </div> 
+            </nav> 
+          </div> 
+        </div> 
+      </div>
+
+
+        <ul class="social social-fixed">
+              <li>
+                <a href="https://www.facebook.com/JainDeemedtobeUniversityofficial">
+                  <i class="socicon-facebook"></i>
+                </a>
+              </li>
+              <li>
+                <a href="https://www.instagram.com/jainuniversityofficial/">
+                  <i class="socicon-instagram"></i>
+                </a>
+              </li>
+              <li>
+                <a href="https://twitter.com/JainDeemedtbUnv">
+                  <i class="socicon-twitter"></i>
+                </a>
+              </li>
+              <li>
+                <a href="https://www.youtube.com/c/JainDeemedtobeUniversity?sub_confirmation=1">
+                  <i class="socicon-youtube"></i>
+                </a>
+              </li>
+              <li>
+                <a href="https://www.linkedin.com/school/jaindeemedtobeuniversity">
+                  <i class="socicon-linkedin"></i>
+                </a>
+              </li>
+        </ul>
+
+
+        <div class="horizontal-nav">
+            <div class="container">
+                <ul id="menu">
+                  <li data-menuanchor="Home" class="active"></li>
+                  <li data-menuanchor="Services"></li>
+                  <li data-menuanchor="Projects"></li>
+                  <li data-menuanchor="Clients"></li>
+                  <li data-menuanchor="Contacts"></li>
+                </ul>
+            </div>
+            <div class="horizontal-controls">
+                <button class="a-pp-prev">prev</button>
+                <span class="divider">|</span>
+                <button class="a-pp-next">next</button>
+            </div>
+        </div>
+
+        <div class="home-horizontal a-horizontal full-height">
+          <div class="section pp-scrollable slide slide1 slide-dark">
+            <div class="slide-container">
+              <div class="slide-bg"><div class="inside"></div></div>
+              <div class="slide-video-container slide-container">
+                <div class="container">
+                  <div class="row slide-content">
+                    <div class="col-sm-7"></div>
+                    <div class="col-sm-5">
+                      <div class="slide-num num">01</div>
+                      <h1 class="slide-title font-teko wow fadeInRight" data-wow-duration="1.2s"><span class="text-primary">An </span><br /> Epitome of the <br /> finest  </h1>
+                      <div class="slide-body wow fadeInRight" data-wow-delay=".2s" data-wow-duration="1.2s">
+                        <h3 class="slide-title-sub font-teko">Academic &amp; Destination</h3>
+                        <div class="slide-descr"></div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="section pp-scrollable slide slide2 slide-dark">
+            <div class="slide-container">
+              <div class="slide-bg"><div class="inside"></div></div>
+              <div class="slide-video-container slide-container">
+                <div class="container">
+                  <div class="row slide-content">
+                    <div class="col-sm-7"></div>
+                    <div class="col-sm-5">
+                      <div class="slide-num num">02</div>
+                      <h1 class="slide-title font-teko wow fadeInRight" data-wow-duration="1.2s"><span class="text-primary">Provides </span><br /> World-Class <br />infrastructure for sports</h1>
+                      <div class="slide-body wow fadeInRight" data-wow-delay=".2s" data-wow-duration="1.2s">
+                        <h3 class="slide-title-sub font-teko"> </h3>
+                        <div class="slide-descr"></div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="section pp-scrollable slide slide3 slide-dark">
+            <div class="slide-container">
+              <div class="slide-bg"><div class="inside"></div></div>
+              <div class="slide-video-container slide-container">
+                <div class="container">
+                  <div class="row slide-content">
+                    <div class="col-sm-7">
+                      <div class="slide-num num">03</div>
+                      <h1 class="slide-title font-teko wow fadeInRight" data-wow-duration="1.2s"><span class="text-primary">Nurture</span><br /> children <br /> to make them</h1>
+                      <div class="slide-body wow fadeInRight" data-wow-delay=".2s" data-wow-duration="1.2s">
+                        <h3 class="slide-title-sub font-teko">Enlightened &amp; Educated individuals</h3>
+                        <div class="slide-descr"></div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="section pp-scrollable slide slide5 slide-dark">
+            <div class="slide-container">
+              <div class="slide-bg"><div class="inside"></div></div>
+              <div class="slide-video-container slide-container">
+                <div class="container">
+                  <div class="row slide-content">
+                    <div class="col-sm-6">
+                      <div class="slide-num num">04</div>
+                      <h1 class="slide-title font-teko wow fadeInRight" data-wow-duration="1.2s"><span class="text-primary">Provides</span><br /> optimum environment <br />  for the kids</h1>
+                      <div class="slide-body wow fadeInRight" data-wow-delay=".2s" data-wow-duration="1.2s">
+                        <h3 class="slide-title-sub font-teko">to explore their creativity</h3>
+                        <a class="btn btn-primary mr-4" href="home.php">Explore<i class="icon ion-ios-arrow-right"></i></a>
+                        <div class="slide-descr"></div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+    </div>
+
+
+    <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/jquery.min.js"><\/script>')</script>
+    <script src="js/smoothscroll.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/animsition.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/wow.min.js"></script>
+    <script src="js/jquery.pagepiling.min.js"></script>
+    <script src="js/isotope.pkgd.min.js"></script>
+    <script src="js/jquery.fancybox.min.js"></script>
+    <script src="js/TweenMax.min.js"></script>
+    <script src="js/ScrollMagic.min.js"></script>
+    <script src="js/animation.gsap.min.js"></script>
+    <script src="js/jquery.viewport.js"></script>
+    <script src="js/jquery.countdown.min.js"></script>
+    <script src="js/script.js"></script>
+    <script src="js/main.js"></script>
+
+    <script>
+      $(document).ready(function() {
+        $('.a-horizontal').pagepiling({
+          scrollingSpeed: 280,
+          menu: '#menu',
+          direction: 'vertical',
+          loopTop: true,
+          loopBottom: true,
+          anchors: ['Home', 'Services', 'Projects', 'Clients', 'Contacts'],
+          navigation: false,
+          afterLoad: function(anchorLink, index){
+            if(index == 1 || index == 2 || index == 3 || index == 4 || index == 5){ // Section with dark bacgkrounds
+              $('body').addClass('dark-horizontal');
+            }
+            else{
+              $('body').removeClass('dark-horizontal');
+            }
+          }
+        });
+
+        $('.a-pp-prev').on('click', function(){
+          $.fn.pagepiling.moveSectionUp();
+        });
+        $('.a-pp-next').on('click', function(){
+          $.fn.pagepiling.moveSectionDown();
+        });
+      });
+    </script>
+
+  </body>
+</html>
