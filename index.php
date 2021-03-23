29f09297104374e4b6f65ea5f10a557c942eb02b
AmruthaBC <ammu14598@gmail.com>
2021-03-23 17:56:32 +0530

    index

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="google" content="notranslate" />
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="icon" type="image/x-icon" href="assets/imgs/fevicon.png"> 
+	<title>Liberty</title>
+	<link rel="stylesheet" type="text/css" href="css/">
+
+</head>
+<body>
+
+<div class="loader"> 
+	<?php include"loader.php"; ?>
+</div> 
+
+
+</body>
+</html>
